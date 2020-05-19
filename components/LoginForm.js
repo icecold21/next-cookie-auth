5635f28cdd27753f6cc188d5c32f58330d1c200e
Jonathan Wylliem <jonathan.wylliem@gmail.com>
2020-05-19 23:05:21 +0800

    Simple login form with backend

---
+++ components/LoginForm.js
@@ -0,0 +1,40 @@
+import { loginUser } from '../lib/auth';
+
+export default class LoginForm extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+
+    const { email, password } = this.state;
+    loginUser(email, password);
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input type="email"
+            name="email"
+            placeholder="email"
+            onChange={this.handleChange} />
+        </div>
+        <div>
+          <input type="password"
+            name="password"
+            placeholder="password"
+            onChange={this.handleChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
